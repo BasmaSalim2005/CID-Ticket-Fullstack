61bf47d3508b8e16a47a8985294619c49aa80f80
basma <bsalim@constructor.university>
2025-07-28 19:25:03 +0100

    back-update

--- Demo1/src/main/java/com/basma/Demo1/config/security/SecurityConfig.java
+++ Demo1/src/main/java/com/basma/Demo1/config/security/SecurityConfig.java
@@ -66,86 +66,88 @@ public class SecurityConfig {
         return http.csrf().disable()
                 .authorizeHttpRequests()
 
-                .requestMatchers( "/api/demo1/users/authenticate","/swagger-ui/**","/swagger-ui/**","/socket/info").permitAll()
+                .requestMatchers( "/api/demo1/users/authenticate", "/api/demo1/applications/apps","/swagger-ui/**","/swagger-ui/**","/socket/info").permitAll()
 
                 .requestMatchers(
                         // USERS
-                        "api/demo1/users/add","api/demo1/users/delete/","api/demo1/users/getall","api/demo1/users/getbyemail/","api/demo1/users/getbyfname/",
-                        "api/demo1/users/getbyid/","api/demo1/users/getdev","api/demo1/users/update1/","api/demo1/users/update1/login",
+                        "/api/demo1/users/add","/api/demo1/users/delete/","/api/demo1/users/getall","/api/demo1/users/getbyemail/","/api/demo1/users/getbyfname/",
+                        "/api/demo1/users/getbyid/","/api/demo1/users/getdev","/api/demo1/users/update1/","/api/demo1/users/update1/login",
                         // APPLICATIONS
-                        "api/demo1/applications/add","api/demo1/applications/activate/","api/demo1/applications/active/byuser/","api/demo1/applications/all",
-                        "api/demo1/applications/app/by-user","api/demo1/applications/apps","api/demo1/applications/countactive","api/demo1/applications/countdisactive",
-                        "api/demo1/applications/countmaintenance","api/demo1/applications/counttotal","api/demo1/applications/counttotalbydev/","api/demo1/applications/delete/",
-                        "api/demo1/applications/disactivate/","api/demo1/applications/disactive/","api/demo1/applications/disactive/byuser","api/demo1/applications/getbyname/",
-                        "api/demo1/applications/inmaintenance/","api/demo1/applications/maintenance/","api/demo1/applications/maintenance/byuser","api/demo1/applications/update/",
-                        "api/demo1/applications/user/add",
+                        "/api/demo1/applications/add","/api/demo1/applications/activate/","/api/demo1/applications/active/byuser/","/api/demo1/applications/all",
+                        "/api/demo1/applications/app/by-user","/api/demo1/applications/apps","/api/demo1/applications/countactive","/api/demo1/applications/countdisactive",
+                        "/api/demo1/applications/countmaintenance","/api/demo1/applications/counttotal","/api/demo1/applications/counttotalbydev/","/api/demo1/applications/delete/",
+                        "/api/demo1/applications/disactivate/","/api/demo1/applications/disactive/","/api/demo1/applications/disactive/byuser","/api/demo1/applications/getbyname/",
+                        "/api/demo1/applications/inmaintenance/","/api/demo1/applications/maintenance/","/api/demo1/applications/maintenance/byuser","/api/demo1/applications/update/",
+                        "/api/demo1/applications/user/add",
                         // TICKETS
-                        "api/demo1/tickets/add","api/demo1/tickets/approve/","api/demo1/tickets/assigned/count/all","api/demo1/tickets/assignedto/",
-                        "api/demo1/tickets/cancel/","api/demo1/tickets/close/","api/demo1/tickets/count/approve","api/demo1/tickets/count/assigne",
-                        "api/demo1/tickets/count/cancel","api/demo1/tickets/count/close","api/demo1/tickets/count/inprogress","api/demo1/tickets/count/solve",
-                        "api/demo1/tickets/delete/","api/demo1/tickets/getall","api/demo1/tickets/getbyemail/","api/demo1/tickets/getclosed/",
-                        "api/demo1/tickets/inprogress/","api/demo1/tickets/notapprove/","api/demo1/tickets/solve/","api/demo1/tickets/status/detail/",
-                        "api/demo1/tickets/update/",
+                        "/api/demo1/tickets/add","/api/demo1/tickets/approve/","/api/demo1/tickets/assigned/count/all","/api/demo1/tickets/assignedto/",
+                        "/api/demo1/tickets/cancel/","/api/demo1/tickets/close/","/api/demo1/tickets/count/approve","/api/demo1/tickets/count/assigne",
+                        "/api/demo1/tickets/count/cancel","/api/demo1/tickets/count/close","/api/demo1/tickets/count/inprogress","/api/demo1/tickets/count/solve",
+                        "/api/demo1/tickets/delete/","/api/demo1/tickets/getall","/api/demo1/tickets/getbyemail/","/api/demo1/tickets/getclosed/",
+                        "/api/demo1/tickets/inprogress/","/api/demo1/tickets/notapprove/","/api/demo1/tickets/solve/","/api/demo1/tickets/status/detail/",
+                        "/api/demo1/tickets/update/",
                         // FEATURES
-                        "api/demo1/features/add","api/demo1/features/delete/","api/demo1/features/getall","api/demo1/features/getbyapp/",
-                        "api/demo1/features/getbyid/","api/demo1/features/update/",
+                        "/api/demo1/features/add","/api/demo1/features/delete/","/api/demo1/features/getall","/api/demo1/features/getbyapp/",
+                        "/api/demo1/features/getbyid/","/api/demo1/features/update/",
                         // FEEDBACK
-                        "api/demo1/feedback/add","api/demo1/feedback/all","api/demo1/feedback/byapp/",
-                        "api/demo1/feedback/delete/","api/demo1/feedback/update/""api/demo1/feedback/",
+                        "/api/demo1/feedback/add","/api/demo1/feedback/all","/api/demo1/feedback/byapp/",
+                        "/api/demo1/feedback/delete/","/api/demo1/feedback/update/","/api/demo1/feedback/"
                 ).hasRole("ADMIN")
                 .requestMatchers(
                         // Features
-                        "api/demo1/features/delete/","api/demo1/features/update/",
-                        "api/demo1/features/getall","api/demo1/features/getbyapp/","api/demo1/features/getbyid/","api/demo1/features/add",
+                        "/api/demo1/features/delete/","/api/demo1/features/update/",
+                        "/api/demo1/features/getall","/api/demo1/features/getbyapp/","/api/demo1/features/getbyid/","/api/demo1/features/add",
                         // Feedback
-                        "api/demo1/feedback/add","api/demo1/feedback/update/",
+                        "/api/demo1/feedback/add","/api/demo1/feedback/update/",
                         // Applications
-                        "api/demo1/applications/add","api/demo1/applications/activate/","api/demo1/applications/active/byuser/",
-                        "api/demo1/applications/getbyname/","api/demo1/applications/apps","api/demo1/applications/countactive",
-                        "api/demo1/applications/countdisactive","api/demo1/applications/countmaintenance","api/demo1/applications/disactivate/",
-                        "api/demo1/applications/disactive/byuser","api/demo1/applications/maintenance/byuser","api/demo1/applications/maintenance/",
-                        "api/demo1/applications/user/add","api/demo1/applications/update/","api/demo1/applications/app/by-user","api/demo1/applications/counttotalbydev/",
-                        "api/demo1/applications/byuser/","api/demo1/applications/app/byuser", 
+                        "/api/demo1/applications/add","/api/demo1/applications/activate/","/api/demo1/applications/active/byuser/",
+                        "/api/demo1/applications/getbyname/","/api/demo1/applications/apps","/api/demo1/applications/countactive",
+                        "/api/demo1/applications/countdisactive","/api/demo1/applications/countmaintenance","/api/demo1/applications/disactivate/",
+                        "/api/demo1/applications/disactive/byuser","/api/demo1/applications/maintenance/byuser","/api/demo1/applications/maintenance/",
+                        "/api/demo1/applications/update/","/api/demo1/applications/app/by-user","/api/demo1/applications/counttotalbydev/",
+                        "/api/demo1/applications/byuser/","/api/demo1/applications/app/byuser", "/api/demo1/applications/apps","/api/demo1/applications/apps",
                         // Tickets
-                        "api/demo1/tickets/update/","api/demo1/tickets/getbyid/","api/demo1/tickets/getclosed/",
-                        "api/demo1/tickets/getbyemail/","api/demo1/tickets/add","api/demo1/tickets/approve/","api/demo1/tickets/assigned/count/all",
-                        "api/demo1/tickets/assignedto/","api/demo1/tickets/approve/","api/demo1/tickets/cancel/","api/demo1/tickets/close/",
-                        "api/demo1/tickets/solve/","api/demo1/tickets/count/inprogress","api/demo1/tickets/count/assigne","api/demo1/tickets/count/approve",
-                        "api/demo1/tickets/count/cancel","api/demo1/tickets/count/close","api/demo1/tickets/count/solve","api/demo1/tickets/getbyemail/",
-                        "api/demo1/tickets/inprogress/","api/demo1/tickets/notapprove/",
+                        "/api/demo1/tickets/update/","/api/demo1/tickets/getbyid/","/api/demo1/tickets/getclosed/",
+                        "/api/demo1/tickets/getbyemail/","/api/demo1/tickets/add","/api/demo1/tickets/approve/","/api/demo1/tickets/assigned/count/all",
+                        "/api/demo1/tickets/assignedto/","/api/demo1/tickets/approve/","/api/demo1/tickets/cancel/","/api/demo1/tickets/close/",
+                        "/api/demo1/tickets/solve/","/api/demo1/tickets/count/inprogress","/api/demo1/tickets/count/assigne","/api/demo1/tickets/count/approve",
+                        "/api/demo1/tickets/count/cancel","/api/demo1/tickets/count/close","/api/demo1/tickets/count/solve","/api/demo1/tickets/getbyemail/",
+                        "/api/demo1/tickets/inprogress/","/api/demo1/tickets/notapprove/"
                         // User endpoints for developer (only login and authentication)
-                        "api/demo1/users/authenticate","api/demo1/users/login"
+                        ,"/api/demo1/users/login"
                 ).hasRole("DEVELOPER")
                 .requestMatchers(
                         // Applications
-                        "api/demo1/applications/all",
+                        "/api/demo1/applications/apps","/api/demo1/applications/all",
                         // Feedback
-                        "api/demo1/feedback/add","api/demo1/feedback/update/",
+                        "/api/demo1/feedback/add","/api/demo1/feedback/update/",
                         // Tickets
-                        "api/demo1/tickets/update/","api/demo1/tickets/getbyid/","api/demo1/tickets/getclosed/",
-                        "api/demo1/tickets/getbyemail/","api/demo1/tickets/add","api/demo1/tickets/approve/","api/demo1/tickets/assigned/count/all",
-                        "api/demo1/tickets/assignedto/","api/demo1/tickets/approve/","api/demo1/tickets/cancel/","api/demo1/tickets/close/",
-                        "api/demo1/tickets/solve/","api/demo1/tickets/count/inprogress","api/demo1/tickets/count/assigne","api/demo1/tickets/count/approve",
-                        "api/demo1/tickets/count/cancel","api/demo1/tickets/count/close","api/demo1/tickets/count/solve","api/demo1/tickets/getbyemail/",
-                        "api/demo1/tickets/inprogress/","api/demo1/tickets/notapprove/",
+                        "/api/demo1/applications/all",
+                        "/api/demo1/tickets/update/","/api/demo1/tickets/getbyid/","/api/demo1/tickets/getclosed/",
+                        "/api/demo1/tickets/getbyemail/","/api/demo1/tickets/add","/api/demo1/tickets/approve/","/api/demo1/tickets/assigned/count/all",
+                        "/api/demo1/tickets/assignedto/","/api/demo1/tickets/approve/","/api/demo1/tickets/cancel/","/api/demo1/tickets/close/",
+                        "/api/demo1/tickets/solve/","/api/demo1/tickets/count/inprogress","/api/demo1/tickets/count/assigne","/api/demo1/tickets/count/approve",
+                        "/api/demo1/tickets/count/cancel","/api/demo1/tickets/count/close","/api/demo1/tickets/count/solve","/api/demo1/tickets/getbyemail/",
+                        "/api/demo1/tickets/inprogress/","/api/demo1/tickets/notapprove/"
                         // User endpoints for technician (only login and authentication)
-                        "api/demo1/users/authenticate","api/demo1/users/login"
+                        ,"/api/demo1/users/login"
                 ).hasRole("TECHNICIAN")
                 .requestMatchers(
                         // APPLICATIONS (only get all)
-                        "api/demo1/applications/all",
+                        "/api/demo1/applications/all",
+                        "/api/demo1/applications/apps",
                         // TICKETS (user actions and user-specific counts only)
-                        "api/demo1/tickets/add", "api/demo1/tickets/update/","api/demo1/tickets/close/",
-                        "api/demo1/tickets/cancel/","api/demo1/tickets/approve/",
-                        "api/demo1/tickets/notapprove/","api/demo1/tickets/getbyid/","api/demo1/tickets/getclosed/",
-                        "api/demo1/tickets/status/detail/","api/demo1/tickets/","api/demo1/tickets/getbyemail/",
+                        "/api/demo1/tickets/add", "/api/demo1/tickets/update/","/api/demo1/tickets/close/",
+                        "/api/demo1/tickets/cancel/","/api/demo1/tickets/approve/",
+                        "/api/demo1/tickets/notapprove/","/api/demo1/tickets/getbyid/","/api/demo1/tickets/getclosed/",
+                        "/api/demo1/tickets/status/detail/","/api/demo1/tickets/","/api/demo1/tickets/getbyemail/",
                         // Only user-specific ticket counts (byuser/byemail)
-                        "api/demo1/tickets/count/approve/byuser","api/demo1/tickets/count/assigne/byuser",
-                        "api/demo1/tickets/count/cancel/byuser","api/demo1/tickets/count/close/byuser",
-                        "api/demo1/tickets/count/inprogress/byuser","api/demo1/tickets/count/solve/byuser",
+                        "/api/demo1/tickets/count/approve/byuser","/api/demo1/tickets/count/assigne/byuser",
+                        "/api/demo1/tickets/count/cancel/byuser","/api/demo1/tickets/count/close/byuser",
+                        "/api/demo1/tickets/count/inprogress/byuser","/api/demo1/tickets/count/solve/byuser",
                         // FEEDBACK (add and update only)
-                        "api/demo1/feedback/add",
-                        "api/demo1/feedback/update/"
+                        "/api/demo1/feedback/add",
+                        "/api/demo1/feedback/update/"
                 ).hasRole("USER")
                 .and()
                 .authorizeHttpRequests()
